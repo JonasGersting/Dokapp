70d43776150ba7737846dbcd7a817a0c2123c71e
Jonas Gersting <johndoe@example.com>
2025-08-25 22:35:55 +0200

    first commit. tailwindcss and daisy ui installed.

---
+++ doc-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button class="btn">Default</button>
+<details class="dropdown">
+  <summary class="btn m-1">open or close</summary>
+  <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</details>
